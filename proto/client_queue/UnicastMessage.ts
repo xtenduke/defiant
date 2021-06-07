cc1fe6a67bf0eab7dc11a01c99a85ed590672083
Jake Laurie <jake.gordon.laurie@gmail.com>
2021-06-07 21:21:14 +1200

    Start filling out meat

---
+++ proto/client_queue/UnicastMessage.ts
@@ -0,0 +1,12 @@
+// Original file: proto/route_client_queue.proto
+
+
+export interface UnicastMessage {
+  'queueId'?: (string);
+  'data'?: (string);
+}
+
+export interface UnicastMessage__Output {
+  'queueId': (string);
+  'data': (string);
+}
--- src/RPCServer.ts
+++ src/RPCServer.ts
@@ -4,7 +4,9 @@ import {PackageDefinition} from "@grpc/grpc-js/build/src/make-client";
 import {ProtoGrpcType} from "../proto/route_client_queue";
 import {AddMessageReply} from "../proto/client_queue/AddMessageReply";
 import {AddMessageRequest} from "../proto/client_queue/AddMessageRequest";
-import {ServerUnaryCall} from "@grpc/grpc-js";
+import {UnicastMessageRequest} from "../proto/client_queue/UnicastMessageRequest";
+import {UnicastMessage} from "../proto/client_queue/UnicastMessage";
+import {ServerUnaryCall, ServerWritableStream} from "@grpc/grpc-js";
 import {Logger} from "./util/Logger";
 
 export class RPCServer {
@@ -12,19 +14,101 @@ export class RPCServer {
     private readonly packageDefinition: PackageDefinition;
     private readonly proto: ProtoGrpcType;
 
+    private readonly listeners: Map<string, Array<ServerWritableStream<UnicastMessageRequest, UnicastMessage>>>;
+    private readonly messages: Array<AddMessageRequest>;
+    private processing: boolean;
+
     public constructor(private readonly port: number) {
         this.packageDefinition = protoLoader.loadSync(this.CLIENT_QUEUE_PROTO_PATH);
         this.proto = grpc.loadPackageDefinition(this.packageDefinition) as unknown as ProtoGrpcType;
+        this.listeners = new Map();
+        this.messages = [];
     }
 
     private AddMessage(
         call: ServerUnaryCall<AddMessageRequest, AddMessageReply>,
         callback: grpc.sendUnaryData<AddMessageReply>
     ) {
+        const queueId = call.request.queueId;
+        if (!queueId) {
+            Logger.error('Someone called [AddMessage] without a queueId');
+
+            // look up how error handling works here
+            callback(null, {
+                message: `missing queueId: ${call.request.data}`,
+                success: false,
+            });
+
+            return;
+        }
+
+        // get queue matching queueId
+        // validate this message & queue
+        this.messages.push(call.request);
+
         callback(null, {
-            message: `you tried to enqueue message with data: ${call.request.data}`,
+            message: `Enqueued message: ${call.request.data}`,
             success: true,
         });
+    }
+
+    // TODO: grpc endpoint for taking receipt of processed message
+
+    private ListenToMessages(
+        call: ServerWritableStream<UnicastMessageRequest, UnicastMessage>,
+    ) {
+        Logger.log('Request for unicast listen on queue:', call.request);
+
+        // perform some validation on our listeners..
+        // eventually negotiate some authorization
+        // -- here 'queues' should be created through other means, i.e. another endpoint -- probably not, due to no persistent storage
+
+        const queueId = call.request.queueId;
+        if (!queueId) {
+            // no error handling for the moment
+            Logger.error('Someone called [ListenToMessages] without a queue id')
+            call.end();
+        }
+
+        let queueListeners = this.listeners.get(queueId);
+        if (!queueListeners) {
+            queueListeners = [];
+        }
+
+        queueListeners.push(call);
+        this.listeners.set(queueId, queueListeners);
+        // begin processing
+        this.processMessages();
+        // not going to call `call.end();` as we will wait for the connection to die, or the client to close
+    }
+
+    public processMessages() {
+        // can't just pop messages off and fire them
+        // we need a way to listen to process receipt from the client
+        // ok for the meantime
+        if (this.processing) {
+            return;
+        } else {
+            this.processing = false;
+        }
+
+        Logger.log('[ProcessMessages] Begin')
+
+        while(this.processing) {
+            // come up with a better way to schedule this
+            setTimeout(() => {
+                const message = this.messages.shift();
+                if (message) {
+                    const listeners = this.listeners.get(message.queueId);
+                    // pick a random listener to send to
+                    const random = Math.floor(Math.random() * this.listeners.size - 1); // can probably think of a more clever way to do this
+                    // listeners could be weighted
+                    const listener = listeners[random];
+                    listener.write(message);
+                }
+            }, 0);
+
+        }
     }
 
     public async startServer(): Promise<void> {
@@ -32,6 +116,7 @@ export class RPCServer {
         const server = new grpc.Server();
         server.addService(this.proto.client_queue.Queue.service, {
             AddMessage: this.AddMessage,
+            ListenToMessages: this.ListenToMessages,
         });
 
         await server.bindAsync(
@@ -47,4 +132,10 @@ export class RPCServer {
                 }
         });
     }
+
+    public async stopProcessing() {
+        this.processing = false;
+    }
+
+    // graceful shutdown would be nice too
 }
