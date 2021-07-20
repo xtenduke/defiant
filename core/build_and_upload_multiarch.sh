2ec88d23a822473d6d56f08339fe91b19a9d67b2
Jake Laurie <jake.gordon.laurie@gmail.com>
2021-07-20 21:11:24 +1200

    Remove armv7 from build

--- core/build_and_upload_multiarch.sh
+++ core/build_and_upload_multiarch.sh
@@ -5,4 +5,4 @@ rev=$(git log -1 --format=%h)
 name="$DOCKER_USERNAME/defiant:$rev"
 echo "building $name"
 
-docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag "$name" .
+docker buildx build --push --platform linux/arm64/v8,linux/amd64 --tag "$name" .
